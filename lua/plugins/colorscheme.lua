8c0292dae219e75940f74ae3b82840b4335f3d1e
Marcus Ortense <marcus.ortense@outlook.com>
2024-11-08 21:59:23 -0300

    feat: rose-pine theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,16 @@ local set_color = function()
 end
 
 return {
-  { 'catppuccin/nvim',      config = set_color },
-  { 'Mofiqul/dracula.nvim', config = set_color },
+  { 'catppuccin/nvim',       config = set_color },
+  { 'Mofiqul/dracula.nvim',  config = set_color },
+  { 'rebelot/kanagawa.nvim', config = set_color },
+  { 'shaunsingh/nord.nvim',  config = set_color },
+  { 'shaunsingh/nord.nvim',  config = set_color },
+  { "rose-pine/neovim",      config = set_color, name = "rose-pine" },
   {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     config = set_color,
   },
-  { 'rebelot/kanagawa.nvim', config = set_color },
-  { 'shaunsingh/nord.nvim',  config = set_color },
 }
