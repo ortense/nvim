16a6f215f2c77e7be1fa250bb86bd5dc0a90fe22
Marcus Ortense <marcus.ortense@outlook.com>
2025-01-19 01:08:45 -0300

    feat: add mini pairs plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,70 +1,52 @@
+local function mini(name, custom)
+  local plugin = {
+    "echasnovski/mini." .. name,
+    version = "*",
+    config = function()
+      require("mini." .. name).setup()
+    end,
+  }
+
+  if type(custom) == "function" then
+    plugin.config = custom
+  elseif type(custom) == "table" then
+    for key, value in pairs(custom) do
+      plugin[key] = value
+    end
+  end
+
+  return plugin
+end
+
 return {
-  {
-    "echasnovski/mini.ai",
-    version = '*',
-    dependencies = {
-      { "echasnovski/mini.nvim", version = false },
-    },
-  },
-  {
-    "echasnovski/mini.move",
-    version = "*",
-    config = function()
-      require("mini.move").setup({
-        mappings = {
-          -- Visual mode
-          left = 'H',
-          right = 'L',
-          down = 'J',
-          up = 'K',
-          -- Normal mode
-          line_left = 'H',
-          line_right = 'L',
-          line_down = 'J',
-          line_up = 'K',
-        },
-        options = {
-          reindent_linewise = true,
-        },
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.cursorword",
-    version = "*",
-    config = function()
-      require("mini.cursorword").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.bracketed",
-    version = "*",
-    config = function()
-      require("mini.bracketed").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.indentscope",
-    version = "*",
-    config = function()
-      require("mini.indentscope").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.surround",
-    version = "*",
-    ops = {},
-    config = function()
-      require("mini.surround").setup()
+  mini("ai"),
+  mini("bracketed"),
+  mini("cursorword"),
+  mini("indentscope"),
+  mini("pairs"),
+  mini("surround"),
+  mini("animate", function()
+    if Config.ui.animateScroll then
+      require("mini.animate").setup()
     end
-  },
-  {
-    "echasnovski/mini.animate",
-    version = "*",
-    config = function()
-      if Config.ui.animateScroll then
-        require("mini.animate").setup()
-      end
-    end
-  },
+  end),
+  mini("move", function()
+    require("mini.move").setup({
+      mappings = {
+        -- Visual mode
+        left = 'H',
+        right = 'L',
+        down = 'J',
+        up = 'K',
+        -- Normal mode
+        line_left = 'H',
+        line_right = 'L',
+        line_down = 'J',
+        line_up = 'K',
+      },
+      options = {
+        reindent_linewise = true,
+      },
+    })
+  end),
 }
